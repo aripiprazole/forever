597fde14ba37419f45b35ad972b6d011cf9a7527
Gabrielle Guimarães de Oliveira <gabrielle1guim@gmail.com>
2023-12-13 12:39:42 -0300

    Use svelte kit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,28 @@
+<script>
+	let value = 'fror';
+
+	$: setInterval(async () => {
+		try {
+			const response = await fetch('/text', {cache: 'no-cache'});
+			const data = await response.text();
+			value = data;
+			console.log('New text loaded', data);
+		} catch {
+			console.error('Failed to load new text');
+		}
+	}, 10000);
+</script>
+
+<svelte:head>
+	<title>@v@</title>
+</svelte:head>
+
+<h1>{value}</h1>
+
+<style>
+  h1 {
+	  color: #fff;
+	  margin: auto;
+	  font-family: Minecraft, sans-serif;
+  }
+</style>
